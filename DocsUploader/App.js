35fc75b42f04a562e989bee07f9729d943d552fe
Hisham Mohammed <hishamcee@gmail.com>
2025-07-30 10:45:47 +0530

    Added navigation between screens

--- DocsUploader/App.js
+++ DocsUploader/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AddScreen from './screens/AddScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import ImageDisplay from './screens/Display/ImageDisplay';
+import PdfDisplay from './screens/Display/PdfDisplay';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="ImageDisplay" component={ImageDisplay} />
+        <Stack.Screen name="PdfDisplay" component={PdfDisplay} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
